7f5220962ba3c4fd54f704158b9c9f08214e257f
Elijah Bansley <elijahbansley@gmail.com>
2021-06-17 13:44:55 -0500

    Add show/hide of vision div

--- badIdeasFrontEndActivites/badIdeasFrontEndActivites/bansley/site.js
+++ badIdeasFrontEndActivites/badIdeasFrontEndActivites/bansley/site.js
@@ -1,1 +1,9 @@
-﻿console.log("Hello World")
+﻿function showHide() {
+    var x = document.getElementById("vision");
+    if (x.style.display !== "none") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
