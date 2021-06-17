cbc28aa64cb95b7c8f1d855f5605b9b04174b47c
Abby Winn <23winna@elmbrookstudents.org>
2021-06-17 13:46:28 -0500

    button

--- badIdeasFrontEndActivites/badIdeasFrontEndActivites/winn/JavaScript.js
+++ badIdeasFrontEndActivites/badIdeasFrontEndActivites/winn/JavaScript.js
@@ -1,1 +1,1 @@
-﻿
+﻿function showHide() {    var x = document.getElementById("vision");    if (x.style.display !== "none") {        x.style.display = "none";    }    else {        x.style.display = "block";    }}
