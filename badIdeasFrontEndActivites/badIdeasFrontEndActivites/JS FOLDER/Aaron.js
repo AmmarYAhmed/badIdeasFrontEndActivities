09530ae5d07fd1814fa29205411a9bb906c01b5c
Aaron Liu <aaronliunj@gmail.com>
2021-06-17 13:46:18 -0500

    Show/HIde div

--- badIdeasFrontEndActivites/badIdeasFrontEndActivites/JS FOLDER/Aaron.js
+++ badIdeasFrontEndActivites/badIdeasFrontEndActivites/JS FOLDER/Aaron.js
@@ -1,1 +1,9 @@
-﻿
+﻿function showHIde() {
+    var x = document.getElementByID("vision");
+    if (x.style !== "none") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
